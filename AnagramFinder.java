f96067d532c02297193eafe5b8bca903d4dfd64e
Megan Wilson <retrorock8821@gmail.com>
2021-04-06 21:51:03 -0500

    merge

--- AnagramFinder.java
+++ AnagramFinder.java
@@ -1,22 +1,13 @@
-
 import java.util.Scanner;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
 import java.util.Arrays;
 
 class AnagramFinder {
-
-  // public AnagramFinder() {
-  // String[] systemArgugments = new String[1];
-  // systemArgugments[0] = "dictionary.txt";
-  // AnagramFinder af = new AnagramFinder(systemArgugments);
-  // }
 
   public AnagramFinder(String[] args) {
     if (args.length > 0) {
@@ -27,11 +18,7 @@ class AnagramFinder {
     }
   }
 
-<<<<<<<
   public static HashMap<String, String> mapByAnagram(Set<String> mySet) {
-=======
-  public HashMap<String, String> mapByAnagram(Set<String> mySet) {
->>>>>>>
     HashMap<String, String> hashByLetters = new HashMap<>();
     for (String word : mySet) {
       hashByLetters.put(word, alphabetize(word));
@@ -39,18 +26,14 @@ class AnagramFinder {
     return hashByLetters;
   }
 
-  public String alphabetize(String word) {
+  public static String alphabetize(String word) {
     char[] charArray = word.toCharArray();
     Arrays.sort(charArray);
     String sortedString = new String(charArray);
     return sortedString;
   }
 
-<<<<<<<
   public static HashMap<String, Integer> readDictionary(String fromPath) { // Fuck String, String
-=======
-  public HashMap<String, Integer> readDictionary(String fromPath) {
->>>>>>>
     HashMap<String, Integer> myDictionary = new HashMap<String, Integer>();
 
     try {
@@ -69,7 +52,7 @@ class AnagramFinder {
     return myDictionary;
   }
 
-  public <K, V> Set<K> getKeys(Map<K, V> map, V value) {
+  public static <K, V> Set<K> getKeys(Map<K, V> map, V value) {
     Set<K> keys = new HashSet<>();
     for (Map.Entry<K, V> entry : map.entrySet()) {
       if (value.equals(entry.getValue())) {
@@ -79,27 +62,19 @@ class AnagramFinder {
     return keys;
   }
 
-<<<<<<<
   public static Set<String> lookup(String word, HashMap<String, Integer> mydict) {
-=======
-  public Set<String> lookup(String word, HashMap<String, Integer> mydict) {
->>>>>>>
     Set<String> subDictionary = getKeys(mydict, word.length());
     HashMap<String, String> subHash = mapByAnagram(subDictionary);
     Set<String> result = lookupAnagram(word, subHash);
     return result;
   }
 
-<<<<<<<
   public static Set<String> lookupAnagram(String word, HashMap<String, String> mydict) {
-=======
-  public Set<String> lookupAnagram(String word, HashMap<String, String> mydict) {
->>>>>>>
     Set<String> setOfAnagrams = getKeys(mydict, alphabetize(word));
     return setOfAnagrams;
   }
 
-  public void play(String txtPath) {
+  public static void play(String txtPath) {
     System.out.println("\nWelcome to Anagram Finder");
     System.out.println("---------------------------");
 
